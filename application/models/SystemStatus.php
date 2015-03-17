8afac79cd0f528a47373238b6c969b72c7cd2897
samah <samah@samah-HP-Pavilion-15-Notebook-PC>
2015-03-17 18:25:17 +0200

    user, thread, systemStatus models

--- application/models/SystemStatus.php
+++ application/models/SystemStatus.php
@@ -1,8 +1,11 @@
 <?php
 
-class Application_Model_SystemStatus
+class Application_Model_SystemStatus extends Zend_Db_Table_Abstract
 {
-
+    function editSystemStatus($data){
+        $this->update($data);
+        return $this->fetchAll()->toArray();
+    }
 
 }
 
--- application/models/Users.php
+++ application/models/Users.php
@@ -1,8 +1,39 @@
 <?php
 
-class Application_Model_Users
+class Application_Model_Users extends Zend_Db_Table_Abstract
 {
-
-
+        protected $_name = "users";
+    
+    function addUser($data){
+        $row = $this->createRow();
+        $row->name = $data['name'];
+        $row->password = md5($data['password']);
+        $row->email = $data['email'];
+        $row->country = $data['country'];
+        $row->signature = $data['signature'];
+        $row->image = $data['image'];
+        $row->gender = $data['gender '];
+        
+        return $row->save();
+    }
+    
+    function listUsers(){
+        
+        return $this->fetchAll()->toArray();
+    }
+    
+    function getUserById($id){
+        return $this->find($id)->toArray();
+    }
+            
+    function editUser($data){
+        $this->update($data, "id=".$data['id']);
+        return $this->fetchAll()->toArray();
+    }
+    
+    function deleteUser($id){
+        return $this->delete("id=$id");
+    }
+    
 }
 
