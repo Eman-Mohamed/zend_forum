6e2cbccde3b0edb3cf9ceee37a839ef583261198
EmanMohamed <ema_shoot@yahoo.com>
2015-03-19 01:03:53 +0200

    adjustment

--- application/controllers/ForumsController.php
+++ application/controllers/ForumsController.php
@@ -12,6 +12,71 @@ class ForumsController extends Zend_Controller_Action
     {
         // action body
     }
+    
+    public function addAction()
+    {
+        $form  = new Application_Form_Forum();
+       
+       if($this->_request->isPost()){
+           if($form->isValid($this->_request->getParams())){
+               $forum_info = $form->getValues();
+               $forum_model = new Application_Model_Forums();
+               $forum_model->addForum($forum_info);
+                       
+           }
+       }
+       
+	$this->view->form = $form;
+    }
+
+    public function deleteAction()
+    {
+        $id = $this->_request->getParam("id");
+        if(!empty($id)){
+            $forum_model = new Application_Model_Forums();
+            $forum_model->deleteForum($id);
+        }
+           $this->redirect("forum/list");
+      
+    }
+
+    public function editAction()
+    {
+        $id = $this->_request->getParam("id");
+        $form  = new Application_Form_Forum();
+        if($this->_request->isPost())
+        {
+           if($form->isValid($this->_request->getParams()))
+            {
+               $forum_info = $form->getValues();
+               $forum_model = new Application_Model_Forums();
+               $forum_model->editForum ($forum_info);                       
+           }
+        }
+        if (!empty($id)) 
+        {
+            $forum_model = new Application_Model_Post();
+            $forum = $forum_model->getPostById($id);
+           //var_dump($forum);
+            $form->populate($forum[0]);
+        } 
+        else 
+        {
+            $this->redirect("forum/list");
+        }  
+        
+        $form->getElement("name")->setRequired(false);
+        $form->getElement("is_locked")->setRequired(false);
+        
+        $this->view->form = $form;
+	$this->render('add');
+    }
+
+    public function listAction()
+    {
+        $forum_model = new Application_Model_Forums();
+        $this->view->forums = $forum_model->listForum();
+    }
 
 
 }
